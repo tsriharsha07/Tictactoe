9d69522ffd75566257cbb14168eb45081ec845e7
Sriharsha <tsriharsha07@gmail.com>
2022-06-15 16:37:17 +0530

    Added square and board

--- src/App.jsx
+++ src/App.jsx
@@ -1,8 +1,14 @@
 import React from "react";
+import Board from "./components/Board";
 
-export default () => (
-  <>
-    <h1>Welcome to React Vite Micro App!</h1>
-    <p>Hard to get more minimal than this React app.</p>
-  </>
-);
+const App = () => {
+  return (
+    <div>
+      <h1>Tic Tac Toe</h1>
+      <Board />
+      
+    </div>
+  );
+};
+
+export default App;
